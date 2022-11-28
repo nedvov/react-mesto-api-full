003b877091b939512cb231ffd880ad16bf27c4fc
Vladimir Nedorezov <nedvov@yandex.ru>
2022-11-28 11:40:02 +0300

    fix: correct cookies

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -119,7 +119,8 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        sameSite: 'none',
+        secure: false,
       });
       res.send({ message: 'Авторизация пройдена успешно' });
     })
