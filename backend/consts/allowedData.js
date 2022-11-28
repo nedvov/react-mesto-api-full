eaa8152f8acf962daeacfc3b6e269634ada54282
Vladimir Nedorezov <nedvov@yandex.ru>
2022-11-28 12:34:21 +0300

    fix: allow url

--- backend/consts/allowedData.js
+++ backend/consts/allowedData.js
@@ -3,6 +3,8 @@ const allowedMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
+  'http://localhost:3000',
+  'https://localhost:3000',
   'localhost:3000',
   'http://nedvov.mesto.nomoredomains.club',
   'https://nedvov.mesto.nomoredomains.club',
